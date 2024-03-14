f3adaa77dec9749cab1f8828fb5fdabce394f952
Program <100379860+ProjectZoo@users.noreply.github.com>
2024-03-14 18:30:02 -0300

    Add files via upload

---
+++ aula05-main/aula04java-main/workspace/BancoVirtual/src/Cliente.java
@@ -0,0 +1,8 @@
+//abrir conta e depositar
+public class Cliente{
+   public String (set titular);
+    nome = JOptionpaine.showInputDialog("Qual o seu nome?");
+}
+
+//herança, polimorfismo
+///e se conta for o getter e o setter do cliente e a agencia das operações?
